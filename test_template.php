17985977d69e550cfe36edd38454f233a0f5dff5
Justin Welenofsky <welenofsky@gmail.com>
2021-02-09 19:58:16 -0800

    Update jQuery

--- test_template.php
+++ test_template.php
@@ -72,9 +72,9 @@ table {
 /* css here */
 
 </style>
-<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script>
-	
+
 //javascript code here
 	
 </script>
